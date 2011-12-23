f2cea84a23a4f610ff1a65df2721e487a725e8c2
RoyAwesome <madKylegar@gmail.com>
2011-12-23 15:29:36 -0800

    Initial Commit

---
+++ src/org/getspout/vanilla/MinecraftPlugin.java
@@ -0,0 +1,19 @@
+package org.getspout.vanilla;
+
+import org.getspout.api.plugin.CommonPlugin;
+
+public class MinecraftPlugin extends CommonPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/org/getspout/vanilla/entities/MinecraftEntityController.java
@@ -0,0 +1,19 @@
+package org.getspout.vanilla.entities;
+
+import org.getspout.api.indev.entity.Controller;
+
+public class MinecraftEntityController extends Controller {
+
+	@Override
+	public void onAttached() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTick(float dt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

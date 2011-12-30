85f9561d17ce1e6bf2c96a01e9772579277ecd1c
RoyAwesome <madKylegar@gmail.com>
2011-12-29 23:10:46 -0800

    Create the sky

--- src/main/java/org/getspout/vanilla/VanillaPlugin.java
+++ src/main/java/org/getspout/vanilla/VanillaPlugin.java
@@ -1,8 +1,13 @@
 package org.getspout.vanilla;
 
 import org.getspout.api.Spout;
+import org.getspout.api.geo.World;
+import org.getspout.api.geo.discrete.Point;
 import org.getspout.api.plugin.CommonPlugin;
 import org.getspout.api.protocol.Protocol;
+import org.getspout.vanilla.entity.sky.NetherSky;
+import org.getspout.vanilla.entity.sky.NormalSky;
+import org.getspout.vanilla.entity.sky.TheEndSky;
 import org.getspout.vanilla.generator.nether.NetherGenerator;
 import org.getspout.vanilla.generator.normal.NormalGenerator;
 import org.getspout.vanilla.generator.theend.TheEndGenerator;
@@ -29,9 +34,15 @@ public class VanillaPlugin extends CommonPlugin {
 
 	@Override
 	public void onEnable() {
-		Spout.getGame().loadWorld("world", new NormalGenerator());
-		Spout.getGame().loadWorld("world_nether", new NetherGenerator());
-		Spout.getGame().loadWorld("world_end", new TheEndGenerator());
+		World normal = Spout.getGame().loadWorld("world", new NormalGenerator());
+		normal.createAndSpawnEntity(new Point(normal,0.f,0.f, 0.f), new NormalSky());
+		
+		World nether = Spout.getGame().loadWorld("world_nether", new NetherGenerator());
+		nether.createAndSpawnEntity(new Point(nether, 0.f, 0.f, 0.f), new NetherSky());
+		
+		World end = Spout.getGame().loadWorld("world_end", new TheEndGenerator());
+		end.createAndSpawnEntity(new Point(end, 0.f, 0.f, 0.f), new TheEndSky());
+		
 		
 	}
 

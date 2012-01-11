2c7bc8a6e431dbb1beaab4bf44b547768cacf739
narrowtux <narrow.m@gmail.com>
2012-01-11 21:27:49 +0100

    Test some cave generation

--- src/main/java/org/spout/vanilla/generator/normal/NormalGenerator.java
+++ src/main/java/org/spout/vanilla/generator/normal/NormalGenerator.java
@@ -16,6 +16,7 @@
  */
 package org.spout.vanilla.generator.normal;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import net.royawesome.jlibnoise.module.source.Perlin;
@@ -27,24 +28,31 @@ import org.spout.vanilla.VanillaBlocks;
 
 public class NormalGenerator implements WorldGenerator {
 	int seed = 42;
-	Perlin noiseHeight = new Perlin();
-	Perlin noiseJitter = new Perlin();
-	Perlin noiseType = new Perlin();
-	Random random = new Random();
+	Perlin layerCount = new Perlin();
+	ArrayList<Perlin> layers = new ArrayList<Perlin>();
+	
 	
 	public NormalGenerator() {
-		noiseHeight.setOctaveCount(5);
-		noiseHeight.setSeed(seed);
-
-		noiseJitter.setOctaveCount(5);
-		noiseJitter.setSeed(seed);
-		
-		noiseType.setOctaveCount(5);
-		noiseType.setSeed(seed);
-		
-		random.setSeed(seed);
+		layerCount.setSeed(seed);
+		layerCount.setOctaveCount(5);
 	}
 
+	public Perlin getLayer(int layer) {
+		if(layer >= 0) {
+			if(layer < layers.size()) {
+				return layers.get(layer);
+			} else {
+				Perlin p = new Perlin();
+				p.setSeed(seed + layer);
+				p.setOctaveCount(5);
+				layers.add(p);
+				return p;
+			}
+		} else {
+			return null;
+		}
+	}
+	
 	private final Populator[] populators = new Populator[]{new TreePopulator(), new PondPopulator(), new StrongholdPopulator(), new VillagePopulator(), new AbandonedMineshaftPopulator(), new DungeonPopulator()};
 	
 	public Populator[] getPopulators() {
@@ -66,46 +74,60 @@ public class NormalGenerator implements WorldGenerator {
 		}
 
 		for (int dx = x; dx < (x+16); dx++) {
-			//for (int dy = y; dy < (y+16); dy++) {
-				for (int dz = z; dz < (z+16); dz++) {
-					short id = 0;
-					double top = (noiseHeight.GetValue(dx / 16.0 + 0.005, 0.05, dz / 16.0 + 0.005) + 1.0) * 4.0 + 60.0;
-					
-					for(int dy = y + 15; dy >= y && dy < y + 16;dy--) {
-						if(dy - 1 == top && dy > 64 && random.nextInt(100) < 40) {
-							//Set a flower
-							if(random.nextBoolean()) {
-								id = VanillaBlocks.dandelion.getId();
-							} else {
-								id = VanillaBlocks.rose.getId();
-							}
-						} else if(dy > top && dy > 64) {
-							id = VanillaBlocks.air.getId();
-						} else if (dy <= 64 && dy > top) {
-							id = VanillaBlocks.water.getId();
-						} else if(dy == (int)top && dy >= 64) {
-							id = VanillaBlocks.grass.getId();
-						} else if(dy + 4 >=(int)top) {
-							id = VanillaBlocks.dirt.getId();
-						} else if(dy != 0){
-							id = VanillaBlocks.stone.getId();
-						} else {
-							id = VanillaBlocks.bedrock.getId();
-						}
-						blockData.set(dx, dy, dz, id);
-					}
-					
-//					if (dy == 64) {
-//						blockData.set(dx, dy, dz, VanillaBlocks.grass.getId());
-//					}
-//					else if (dy < 64 && dy >= 60) {
-//						blockData.set(dx, dy, dz, VanillaBlocks.dirt.getId());
-//					}
-//					else if (dy < 60) {
-//						blockData.set(dx, dy, dz, VanillaBlocks.stone.getId());
-//					}
+			for (int dz = z; dz < (z+16); dz++) {
+				int layers = (int) ((layerCount.GetValue(dx / 16.0 + 0.05, 0.05, dz / 16.0 + 0.05) + 1.0) * 10.0 + 2);
+				
+				if(layers <= 0) {
+					layers = 2;
 				}
-			//}
+				
+				int heightPerLayer = 128/layers;
+				
+				for (int layer = 0; layer < layers; layer+=2) {
+					Perlin bottom = getLayer(layer);
+					Perlin top = getLayer(layer+1);
+					int min = heightPerLayer * layer;
+					int max = heightPerLayer * (layer + 1);
+					int b = (int) ((getPerlinValueXZ(bottom, dx, dz) + 1.0) * heightPerLayer / 3.0 + min);
+					int t = (int) ((getPerlinValueXZ(top, dx, dz) + 1.0) * heightPerLayer / 3.0 + max);
+					if(b < t) {
+						for(int dy = y; dy < y + 16; dy++) {
+							short id = VanillaBlocks.air.getId();
+							if(dy <= b || dy >= t) {
+								id = VanillaBlocks.stone.getId();
+							}
+							
+							if(dy < 66 && dy > 62 && dx < 16 && dx > -16 && dz < 16 && dz > -16) {
+								id = 0;
+							}
+							blockData.set(dx, dy, dz, id);
+						}
+					}
+				}
+				
+			}
 		}
+	}
+	
+	public static double getPerlinValueXZ(Perlin perlin, int x, int z) {
+		return perlin.GetValue(x / 16.0 + 0.05, 0.05, z / 16.0 + 0.05);
+	}
+ 
+	private short getBlockId(int top, int dy) {
+		short id;
+		if(dy > top && dy > 64) {
+			id = VanillaBlocks.air.getId();
+		} else if (dy <= 64 && dy > top) {
+			id = VanillaBlocks.water.getId();
+		} else if(dy == (int)top && dy >= 64) {
+			id = VanillaBlocks.grass.getId();
+		} else if(dy + 4 >=(int)top) {
+			id = VanillaBlocks.dirt.getId();
+		} else if(dy != 0){
+			id = VanillaBlocks.stone.getId();
+		} else {
+			id = VanillaBlocks.bedrock.getId();
+		}
+		return id;
 	}
 }

04e049b34d0ce2ee14f97a563456374fcee1261a
Walker <wjcrouse913@aim.com>
2012-01-28 13:40:42 -0500

    Basic tree generation

--- src/main/java/org/spout/vanilla/generator/normal/TreePopulator.java
+++ src/main/java/org/spout/vanilla/generator/normal/TreePopulator.java
@@ -33,124 +33,123 @@ import org.spout.api.geo.discrete.Point;
 import org.spout.vanilla.VanillaBlocks;
 
 public class TreePopulator implements Populator {
-    
-    private Random random = new Random();
+	
+	private Random random = new Random();
     
     public void generateNormalTree(Chunk c, Point point) {
-        int x = (int) point.getX();
-        int y = (int) point.getY();
-        int z = (int) point.getZ();
-        short wood = VanillaBlocks.log.getId();
-        short leaves = VanillaBlocks.leaves.getId();
-        World world = c.getWorld();
+    	int x = (int) point.getX();
+    	int y = (int) point.getY();
+    	int z = (int) point.getZ();
+    	short wood = VanillaBlocks.log.getId();
+    	short leaves = VanillaBlocks.leaves.getId();
+    	World world = c.getWorld();
+    	
+    	// Trunk
+    	c.setBlockId(x, y+1, z, wood, world);
+    	c.setBlockId(x, y+2, z, wood, world);
+    	c.setBlockId(x, y+3, z, wood, world);
+    	c.setBlockId(x, y+4, z, wood, world);
+    	c.setBlockId(x, y+5, z, wood, world);
+    	c.setBlockId(x, y+6, z, wood, world);
         
-        // Trunk
-        c.setBlockId(x, y+1, z, wood, world);
-        c.setBlockId(x, y+2, z, wood, world);
-        c.setBlockId(x, y+3, z, wood, world);
-        c.setBlockId(x, y+4, z, wood, world);
-        c.setBlockId(x, y+5, z, wood, world);
-        c.setBlockId(x, y+6, z, wood, world);
+    	// Top leaves layer
+    	c.setBlockId(x, y+7, z, leaves, world);
+    	c.setBlockId(x+1, y+7, z, leaves, world);
+    	c.setBlockId(x-1, y+7, z, leaves, world);
+    	c.setBlockId(x, y+7, z+1, leaves, world);
+    	c.setBlockId(x, y+7, z-1, leaves, world);
         
-        // Top leaves layer
-        c.setBlockId(x, y+7, z, leaves, world);
-        c.setBlockId(x+1, y+7, z, leaves, world);
-        c.setBlockId(x-1, y+7, z, leaves, world);
-        c.setBlockId(x, y+7, z+1, leaves, world);
-        c.setBlockId(x, y+7, z-1, leaves, world);
-        
-        // Second leaves layer
-        c.setBlockId(x+1, y+6, z, leaves, world);
-        c.setBlockId(x-1, y+6, z, leaves, world);
-        c.setBlockId(x, y+6, z+1, leaves, world);
-        c.setBlockId(x, y+6, z-1, leaves, world);
-        c.setBlockId(x+1, y+6, z+1, leaves, world);
-        c.setBlockId(x+1, y+6, z-1, leaves, world);
-        c.setBlockId(x-1, y+6, z-1, leaves, world);
-        c.setBlockId(x-1, y+6, z+1, leaves, world);
-        
-        
-        // Third leaves layer
-        c.setBlockId(x+1, y+5, z, leaves, world);
-        c.setBlockId(x-1, y+5, z, leaves, world);
-        c.setBlockId(x, y+5, z+1, leaves, world);
-        c.setBlockId(x, y+5, z-1, leaves, world);
-        c.setBlockId(x+1, y+5, z+1, leaves, world);
-        c.setBlockId(x+1, y+5, z-1, leaves, world);
-        c.setBlockId(x-1, y+5, z-1, leaves, world);
-        c.setBlockId(x-1, y+5, z+1, leaves, world);
-        c.setBlockId(x+2, y+5, z, leaves, world);
-        c.setBlockId(x-2, y+5, z, leaves, world);
-        c.setBlockId(x, y+5, z+2, leaves, world);
-        c.setBlockId(x, y+5, z-2, leaves, world);
-        c.setBlockId(x+2, y+5, z+2, leaves, world);
-        c.setBlockId(x+2, y+5, z-2, leaves, world);
-        c.setBlockId(x+2, y+5, z-2, leaves, world);
-        c.setBlockId(x+2, y+5, z+2, leaves, world);
-        c.setBlockId(x+2, y+5, z-1, leaves, world);
-        c.setBlockId(x+1, y+5, z-2, leaves, world);
-        c.setBlockId(x-1, y+5, z-2, leaves, world);
-        c.setBlockId(x-2, y+5, z-1, leaves, world);
-        c.setBlockId(x-2, y+5, z+1, leaves, world);
-        c.setBlockId(x-1, y+5, z+2, leaves, world);
-        c.setBlockId(x+1, y+5, z+2, leaves, world);
-        c.setBlockId(x+2, y+5, z+1, leaves, world);
-        
-        // Fourth leaves layer
-        c.setBlockId(x+1, y+4, z, leaves, world);
-        c.setBlockId(x-1, y+4, z, leaves, world);
-        c.setBlockId(x, y+4, z+1, leaves, world);
-        c.setBlockId(x, y+4, z-1, leaves, world);
-        c.setBlockId(x+1, y+4, z+1, leaves, world);
-        c.setBlockId(x+1, y+4, z-1, leaves, world);
-        c.setBlockId(x-1, y+4, z-1, leaves, world);
-        c.setBlockId(x-1, y+4, z+1, leaves, world);
-        c.setBlockId(x+2, y+4, z, leaves, world);
-        c.setBlockId(x-2, y+4, z, leaves, world);
-        c.setBlockId(x, y+4, z+2, leaves, world);
-        c.setBlockId(x, y+4, z-2, leaves, world);
-        c.setBlockId(x+2, y+4, z+2, leaves, world);
-        c.setBlockId(x+2, y+4, z-2, leaves, world);
-        c.setBlockId(x+2, y+4, z-2, leaves, world);
-        c.setBlockId(x+2, y+4, z+2, leaves, world);
-        c.setBlockId(x+2, y+4, z-1, leaves, world);
-        c.setBlockId(x+1, y+4, z-2, leaves, world);
-        c.setBlockId(x-1, y+4, z-2, leaves, world);
-        c.setBlockId(x-2, y+4, z-1, leaves, world);
-        c.setBlockId(x-2, y+4, z+1, leaves, world);
-        c.setBlockId(x-1, y+4, z+2, leaves, world);
-        c.setBlockId(x+1, y+4, z+2, leaves, world);
-        c.setBlockId(x+2, y+4, z+1, leaves, world);
-	}
+    	// Second leaves layer
+    	c.setBlockId(x+1, y+6, z, leaves, world);
+    	c.setBlockId(x-1, y+6, z, leaves, world);
+    	c.setBlockId(x, y+6, z+1, leaves, world);
+    	c.setBlockId(x, y+6, z-1, leaves, world);
+    	c.setBlockId(x+1, y+6, z+1, leaves, world);
+    	c.setBlockId(x+1, y+6, z-1, leaves, world);
+    	c.setBlockId(x-1, y+6, z-1, leaves, world);
+    	c.setBlockId(x-1, y+6, z+1, leaves, world);
+    	
+    	// Third leaves layer
+    	c.setBlockId(x+1, y+5, z, leaves, world);
+    	c.setBlockId(x-1, y+5, z, leaves, world);
+    	c.setBlockId(x, y+5, z+1, leaves, world);
+    	c.setBlockId(x, y+5, z-1, leaves, world);
+    	c.setBlockId(x+1, y+5, z+1, leaves, world);
+    	c.setBlockId(x+1, y+5, z-1, leaves, world);
+    	c.setBlockId(x-1, y+5, z-1, leaves, world);
+    	c.setBlockId(x-1, y+5, z+1, leaves, world);
+    	c.setBlockId(x+2, y+5, z, leaves, world);
+    	c.setBlockId(x-2, y+5, z, leaves, world);
+    	c.setBlockId(x, y+5, z+2, leaves, world);
+    	c.setBlockId(x, y+5, z-2, leaves, world);
+    	c.setBlockId(x+2, y+5, z+2, leaves, world);
+    	c.setBlockId(x+2, y+5, z-2, leaves, world);
+    	c.setBlockId(x+2, y+5, z-2, leaves, world);
+    	c.setBlockId(x+2, y+5, z+2, leaves, world);
+    	c.setBlockId(x+2, y+5, z-1, leaves, world);
+    	c.setBlockId(x+1, y+5, z-2, leaves, world);
+    	c.setBlockId(x-1, y+5, z-2, leaves, world);
+    	c.setBlockId(x-2, y+5, z-1, leaves, world);
+    	c.setBlockId(x-2, y+5, z+1, leaves, world);
+    	c.setBlockId(x-1, y+5, z+2, leaves, world);
+    	c.setBlockId(x+1, y+5, z+2, leaves, world);
+    	c.setBlockId(x+2, y+5, z+1, leaves, world);
+    	
+    	// Fourth leaves layer
+    	c.setBlockId(x+1, y+4, z, leaves, world);
+    	c.setBlockId(x-1, y+4, z, leaves, world);
+    	c.setBlockId(x, y+4, z+1, leaves, world);
+    	c.setBlockId(x, y+4, z-1, leaves, world);
+    	c.setBlockId(x+1, y+4, z+1, leaves, world);
+    	c.setBlockId(x+1, y+4, z-1, leaves, world);
+    	c.setBlockId(x-1, y+4, z-1, leaves, world);
+    	c.setBlockId(x-1, y+4, z+1, leaves, world);
+    	c.setBlockId(x+2, y+4, z, leaves, world);
+    	c.setBlockId(x-2, y+4, z, leaves, world);
+    	c.setBlockId(x, y+4, z+2, leaves, world);
+    	c.setBlockId(x, y+4, z-2, leaves, world);
+    	c.setBlockId(x+2, y+4, z+2, leaves, world);
+    	c.setBlockId(x+2, y+4, z-2, leaves, world);
+    	c.setBlockId(x+2, y+4, z-2, leaves, world);
+    	c.setBlockId(x+2, y+4, z+2, leaves, world);
+    	c.setBlockId(x+2, y+4, z-1, leaves, world);
+    	c.setBlockId(x+1, y+4, z-2, leaves, world);
+    	c.setBlockId(x-1, y+4, z-2, leaves, world);
+    	c.setBlockId(x-2, y+4, z-1, leaves, world);
+    	c.setBlockId(x-2, y+4, z+1, leaves, world);
+    	c.setBlockId(x-1, y+4, z+2, leaves, world);
+    	c.setBlockId(x+1, y+4, z+2, leaves, world);
+    	c.setBlockId(x+2, y+4, z+1, leaves, world);
+    }
     
     public void generateBigTree(Chunk c, Point p) {
-        
+    	
     }
     
     public void generateRedwoodTree(Chunk c, Point p) {
-    
+    	
     }
     
     public void generateBirchTree(Chunk c, Point point) {
-    
+    	
     }
     
     @Override
     public void populate(Chunk c) {
-        for (int a = 0; a < random.nextInt(20); a++) {
-            int x = random.nextInt(16);
-            int z = random.nextInt(16);
-            int y = 0;
-            for (int yy = 128; yy > 0; yy--) {
-                if (c.getBlockId(x, yy, z) == 2 && c.getBlockId(y, yy, z) != 0) {
-                    y = yy;
-                    break;
-                }
-            }
-            
-            if (c.getBlockId(x, y, z) == 2) {
-                this.generateNormalTree(c, new Point(c.getWorld(), x, y, z));
-            }
-        }
+    	for (int a = 0; a < random.nextInt(20); a++) {
+    		int x = random.nextInt(16);
+    		int z = random.nextInt(16);
+    		int y = 0;
+    		for (int yy = 128; yy > 0; yy--) {
+    			if (c.getBlockId(x, yy, z) == 2 && c.getBlockId(y, yy, z) != 0) {
+    				y = yy;
+    				break;
+    			}
+    		}
+    		
+    		if (c.getBlockId(x, y, z) == 2) {
+    			this.generateNormalTree(c, new Point(c.getWorld(), x, y, z));
+    		}
+    	}
     }
 }

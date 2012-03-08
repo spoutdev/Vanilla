7709cef71b24bd4cd387123996ab76838eb987ae
Chase W <chasew@silkweaver.com>
2012-03-07 21:10:00 -0500

    More block drops. Giant mushrooms are a big buggy when destroying them too fast (like with an axe)
    Signed-off-by: Chase W <chasew@silkweaver.com>

---
+++ src/main/java/org/spout/vanilla/material/block/WheatCrop.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of Vanilla (http://www.spout.org/).
+ *
+ * Vanilla is licensed under the SpoutDev License Version 1.
+ *
+ * Vanilla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the SpoutDev License Version 1.
+ *
+ * Vanilla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the SpoutDev License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://www.spout.org/SpoutDevLicenseV1.txt> for the full license,
+ * including the MIT license.
+ */
+package org.spout.vanilla.material.block;
+
+import java.util.Random;
+
+import org.spout.vanilla.material.Plant;
+import org.spout.vanilla.material.block.Solid;
+
+public class WheatCrop extends Solid implements Plant {
+	private Random rand = new Random();
+	
+	public WheatCrop(String name) {
+		super(name, 59, 0x0);
+	}
+	
+	@Override
+	public boolean hasGrowthStages() {
+		return true;
+	}
+
+	@Override
+	public int getNumGrowthStages() {
+		return 8;
+	}
+
+	@Override
+	public int getMinimumLightToGrow() {
+		return 9;
+	}
+
+	@Override
+	public int getDrop() {
+		return 295;
+	}
+	
+	@Override
+	public int getDropCount() {
+		return rand.nextInt(4);
+	}
+	
+	// TODO: Grow
+	// TODO: Trampling
+	// TODO: Multiple drops
+}

f6582261987a0e4e88df3adb3826c7ea7597c1ba
Devon Endicott <dephurites@gmail.com>
2012-03-22 18:08:24 -0400

    Base of swamp biome, added trees with vines
    Signed-off-by: Devon Endicott <dephurites@gmail.com>

--- src/main/java/org/spout/vanilla/generator/normal/biome/SwampBiome.java
+++ src/main/java/org/spout/vanilla/generator/normal/biome/SwampBiome.java
@@ -25,22 +25,83 @@
  */
 package org.spout.vanilla.generator.normal.biome;
 
+import net.royawesome.jlibnoise.NoiseQuality;
+import net.royawesome.jlibnoise.module.modifier.Turbulence;
+import net.royawesome.jlibnoise.module.source.Perlin;
+
 import org.spout.api.util.cuboid.CuboidShortBuffer;
 
+import org.spout.vanilla.VanillaMaterials;
 import org.spout.vanilla.generator.VanillaBiomeType;
+import org.spout.vanilla.generator.normal.decorator.CactusDecorator;
+import org.spout.vanilla.generator.normal.decorator.CaveDecorator;
+import org.spout.vanilla.generator.normal.decorator.DungeonDecorator;
+import org.spout.vanilla.generator.normal.decorator.GrassDecorator;
+import org.spout.vanilla.generator.normal.decorator.MushroomDecorator;
+import org.spout.vanilla.generator.normal.decorator.OreDecorator;
 
 public class SwampBiome extends VanillaBiomeType {
+	private Perlin base = new Perlin();
+	private Turbulence noise = new Turbulence();
+
 	public SwampBiome() {
-		super(6);
+		super(6, new CactusDecorator(), new MushroomDecorator(), new GrassDecorator(), new OreDecorator(), new CaveDecorator(), new DungeonDecorator());
+		base.setNoiseQuality(NoiseQuality.BEST);
+		base.setOctaveCount(6);
+		base.setFrequency(0.3);
+		base.setPersistence(0.12);
+		base.setLacunarity(0.5);
+		noise.SetSourceModule(0, base);
+		noise.setFrequency(0.3);
+		noise.setRoughness(1);
+		noise.setPower(0.5);
 	}
 
 	@Override
 	public void generateColumn(CuboidShortBuffer blockData, int x, int chunkY, int z) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		base.setSeed((int) blockData.getWorld().getSeed());
+		noise.setSeed((int) blockData.getWorld().getSeed());
+
+		final int y = chunkY * 16;
+		final int height = (int) ((noise.GetValue(x / 16.0 + 0.005, 0.05, z / 16.0 + 0.005) + 1.0) * 4.0 + 60.0);
+
+		for (int dy = y; dy < y + 16; dy++) {
+			blockData.set(x, dy, z, getBlockId(height, dy));
+		}
+
+		if (height < 64) {
+			generateWateredStack(blockData, x, y, z, 64);
+		}
 	}
 
 	@Override
 	public String getName() {
 		return "Swamp";
+	}
+
+	protected void generateWateredStack(CuboidShortBuffer blockData, int x, int y, int z, int maxSeaLevel) {
+		for (int dy = y + 15; dy >= y; dy--) {
+			if (dy < maxSeaLevel && blockData.get(x, dy, z) == VanillaMaterials.AIR.getId()) {
+				blockData.set(x, dy, z, VanillaMaterials.WATER.getId());
+			} else {
+				break;
+			}
+		}
+	}
+
+	protected short getBlockId(int top, int dy) {
+		short id;
+		if (dy > top) {
+			id = VanillaMaterials.AIR.getId();
+		} else if (dy == top && dy >= 63) {
+			id = VanillaMaterials.GRASS.getId();
+		} else if (dy + 4 >= top) {
+			id = VanillaMaterials.DIRT.getId();
+		} else if (dy != 0) {
+			id = VanillaMaterials.STONE.getId();
+		} else {
+			id = VanillaMaterials.BEDROCK.getId();
+		}
+		return id;
 	}
 }

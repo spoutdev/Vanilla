ba791fa5097f34e188b0f6fa42b1dfc6883dc96c
Aleksi Sapon <QcTechs@gmail.com>
2012-04-24 21:44:24 -0400

    Added well generation and populating code
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

--- src/main/java/org/spout/vanilla/generator/normal/decorator/WellDecorator.java
+++ src/main/java/org/spout/vanilla/generator/normal/decorator/WellDecorator.java
@@ -28,14 +28,106 @@ package org.spout.vanilla.generator.normal.decorator;
 import java.util.Random;
 
 import org.spout.api.generator.biome.BiomeDecorator;
+import org.spout.api.geo.World;
+import org.spout.api.geo.cuboid.Block;
 import org.spout.api.geo.cuboid.Chunk;
+import org.spout.api.material.block.BlockFace;
+import org.spout.vanilla.material.VanillaMaterials;
 
 /**
  * Decorator that decorates a biome with a well structure.
  */
 public class WellDecorator implements BiomeDecorator {
+
+	// generation odd, 'ODD' chunk per chunk
+	private static final short ODD = 1000;
+
 	@Override
 	public void populate(Chunk chunk, Random random) {
+		if (chunk.getY() < 4) {
+			return;
+		}
+		if (random.nextInt(ODD) != 0) {
+			return;
+		}
+		final int x = random.nextInt(16) + 8;
+		final int z = random.nextInt(16) + 8;
+		final int y = getHighestWorkableBlock(chunk, x, z);
+		if (y == -1) {
+			return;
+		}
+		generateDesertWell(chunk.getWorld(), chunk.getX() * 16 + x, chunk.getY() * 16 + y, chunk.getZ() * 16 + z);
+	}
 
+	private void generateDesertWell(World world, int x, int y, int z) {
+		y -= 1;
+		if (world.getBlockMaterial(x, y, z) != VanillaMaterials.SAND) {
+			return;
+		}
+		if (!canBuildDesertWell(world, x, y, z)) {
+			return;
+		}
+		for (int xx = x - 2; xx < x + 3; xx++) {
+			for (int zz = z - 2; zz < z + 3; zz++) {
+				for (int yy = y - 1; yy < y + 5; yy++) {
+					if (yy > y - 2 && yy < y + 1) {
+						world.setBlockMaterial(xx, yy, zz, VanillaMaterials.SANDSTONE, (short) 0, false, world);
+					}
+					if (yy == y) {
+						if ((xx == x && z == zz)
+								|| ((xx == x - 1 || xx == x + 1) && zz == z)
+								|| ((zz == z + 1 || zz == z - 1) && x == xx)) {
+							world.setBlockMaterial(xx, yy, zz, VanillaMaterials.WATER, (short) 0, false, world);
+						}
+					}
+					if (yy == y + 1) {
+						if (xx == x - 2 || xx == x + 2 || zz == z - 2 || zz == z + 2) {
+							world.setBlockMaterial(xx, yy, zz, VanillaMaterials.SANDSTONE, (short) 0, false, world);
+						}
+						if (((xx == x - 2 || xx == x + 2) && zz == z)
+								|| ((zz == z + 2 || zz == z - 2) && x == xx)) {
+							world.setBlockMaterial(xx, yy, zz, VanillaMaterials.SLAB, (short) 1, false, world);
+						}
+					}
+					if (yy == y + 4 && xx > x - 2 && xx < x + 2 && zz > z - 2 && zz < z + 2) {
+						if (xx == 0 && zz == 0) {
+							world.setBlockMaterial(xx, yy, zz, VanillaMaterials.SANDSTONE, (short) 0, false, world);
+						} else {
+							world.setBlockMaterial(xx, yy, zz, VanillaMaterials.SLAB, (short) 1, false, world);
+						}
+					}
+					if (yy > y && yy < y + 4) {
+						if ((xx == x - 1 || xx == x + 1) && (zz == z + 1 || zz == z - 1)) {
+							world.setBlockMaterial(xx, yy, zz, VanillaMaterials.SANDSTONE, (short) 0, false, world);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private boolean canBuildDesertWell(World world, int x, int y, int z) {
+		for (int xx = x - 2; xx < x + 3; xx++) {
+			for (int zz = z - 2; zz < z + 3; zz++) {
+				Block block = world.getBlock(xx, y - 1, zz);
+				if (block.getMaterial() == VanillaMaterials.AIR
+						&& block.move(BlockFace.BOTTOM).getMaterial() == VanillaMaterials.AIR) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private int getHighestWorkableBlock(Chunk chunk, int x, int z) {
+		int y = 15;
+		while (chunk.getBlockMaterial(x, y, z) == VanillaMaterials.AIR) {
+			y--;
+			if (y == 0 || chunk.getBlockMaterial(x, y, z) == VanillaMaterials.WATER) {
+				return -1;
+			}
+		}
+		y++;
+		return y;
 	}
 }

be0b3fbb68fbd9a475b7b90cd467b8f6c49acc5a
atheriel <atherielx@gmail.com>
2012-05-03 03:16:59 +0200

    Added a proper biome selector and a few generation tweaks.
    Also, (1) Implemented a workaround for having generators set their own
    spawns. It's hack-y, but it'll have to do for now; and (2) implemented
    a basic "/biome" command, mostly to show off the fact that the selector
    is working. Try it out.

--- src/main/java/org/spout/vanilla/generator/normal/biome/SmallMountainsBiome.java
+++ src/main/java/org/spout/vanilla/generator/normal/biome/SmallMountainsBiome.java
@@ -27,9 +27,7 @@ package org.spout.vanilla.generator.normal.biome;
 
 import net.royawesome.jlibnoise.NoiseQuality;
 import net.royawesome.jlibnoise.module.source.RidgedMulti;
-
 import org.spout.api.util.cuboid.CuboidShortBuffer;
-
 import org.spout.vanilla.generator.VanillaBiomeType;
 import org.spout.vanilla.generator.normal.decorator.CaveDecorator;
 import org.spout.vanilla.generator.normal.decorator.FlowerDecorator;
@@ -37,47 +35,48 @@ import org.spout.vanilla.generator.normal.decorator.OreDecorator;
 import org.spout.vanilla.generator.normal.decorator.TreeDecorator;
 import org.spout.vanilla.material.VanillaMaterials;
 
-public class MountainBiome extends VanillaBiomeType {
-	private RidgedMulti noise = new RidgedMulti();
+public class SmallMountainsBiome extends VanillaBiomeType {
 
-	public MountainBiome() {
-		super(3, new FlowerDecorator(), new TreeDecorator(), new CaveDecorator(), new OreDecorator());
-		noise.setNoiseQuality(NoiseQuality.BEST);
-		noise.setOctaveCount(10);
-		noise.setFrequency(0.4);
-		noise.setLacunarity(0.10);
-	}
+    private RidgedMulti noise = new RidgedMulti();
 
-	@Override
-	public void generateColumn(CuboidShortBuffer blockData, int x, int chunkY, int z) {
-		noise.setSeed((int) blockData.getWorld().getSeed());
+    public SmallMountainsBiome(int biomeId) {
+        super(biomeId, new FlowerDecorator(), new TreeDecorator(), new CaveDecorator(), new OreDecorator());
+        noise.setNoiseQuality(NoiseQuality.BEST);
+        noise.setOctaveCount(10);
+        noise.setFrequency(0.4);
+        noise.setLacunarity(0.10);
+    }
 
-		final int y = chunkY * 16;
-		final int height = (int) ((noise.GetValue(x / 16.0 + 0.005, 0.05, z / 16.0 + 0.005) + 1.0) * 10.0 + 60.0);
+    @Override
+    public void generateColumn(CuboidShortBuffer blockData, int x, int chunkY, int z) {
+        noise.setSeed((int) blockData.getWorld().getSeed());
 
-		for (int dy = y; dy < y + 16; dy++) {
-			blockData.set(x, dy, z, getBlockId(height, dy));
-		}
-	}
+        final int y = chunkY * 16;
+        final int height = (int) ((noise.GetValue(x / 16.0 + 0.005, 0.05, z / 16.0 + 0.005) + 1.0) * 5.0 + 60.0);
 
-	@Override
-	public String getName() {
-		return "Mountains";
-	}
+        for (int dy = y; dy < y + 16; dy++) {
+            blockData.set(x, dy, z, getBlockId(height, dy));
+        }
+    }
 
-	protected short getBlockId(int top, int dy) {
-		short id;
-		if (dy > top) {
-			id = VanillaMaterials.AIR.getId();
-		} else if (dy == top && dy >= 63) {
-			id = VanillaMaterials.GRASS.getId();
-		} else if (dy + 4 >= top) {
-			id = VanillaMaterials.DIRT.getId();
-		} else if (dy != 0) {
-			id = VanillaMaterials.STONE.getId();
-		} else {
-			id = VanillaMaterials.BEDROCK.getId();
-		}
-		return id;
-	}
+    @Override
+    public String getName() {
+        return "Small Mountains";
+    }
+
+    protected short getBlockId(int top, int dy) {
+        short id;
+        if (dy > top) {
+            id = VanillaMaterials.AIR.getId();
+        } else if (dy == top && dy >= 63) {
+            id = VanillaMaterials.GRASS.getId();
+        } else if (dy + 4 >= top) {
+            id = VanillaMaterials.DIRT.getId();
+        } else if (dy != 0) {
+            id = VanillaMaterials.STONE.getId();
+        } else {
+            id = VanillaMaterials.BEDROCK.getId();
+        }
+        return id;
+    }
 }

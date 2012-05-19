c8415932d63c1602d9ba13d70bd0f144943f96f4
Chris Sanders <NinjaZidane@hotmail.com>
2012-05-19 03:34:35 -0400

    Shears should take away durability too.
    Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

--- src/main/java/org/spout/vanilla/material/item/misc/Shears.java
+++ src/main/java/org/spout/vanilla/material/item/misc/Shears.java
@@ -36,9 +36,10 @@ import org.spout.vanilla.controller.living.creature.passive.Sheep;
 import org.spout.vanilla.controller.living.player.VanillaPlayer;
 import org.spout.vanilla.controller.object.moving.Item;
 import org.spout.vanilla.material.VanillaMaterials;
+import org.spout.vanilla.material.item.MiningTool;
 import org.spout.vanilla.material.item.Tool;
 
-public class Shears extends Tool {
+public class Shears extends MiningTool {
 	private Random rand = new Random();
 
 	public Shears(String name, int id, short durability) {

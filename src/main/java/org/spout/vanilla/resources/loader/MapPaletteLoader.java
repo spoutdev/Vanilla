c9fbf6d680477021d41fc82d9222ed44734f0f63
Irmo van den Berge <bergerkiller@gmail.com>
2012-06-25 14:24:51 +0200

    Fixed map palette headers
    Signed-off-by: Irmo van den Berge <bergerkiller@gmail.com>

--- src/main/java/org/spout/vanilla/resources/loader/MapPaletteLoader.java
+++ src/main/java/org/spout/vanilla/resources/loader/MapPaletteLoader.java
@@ -1,10 +1,10 @@
 /*
- * This file is part of SpoutAPI.
+ * This file is part of Vanilla.
  *
- * Copyright (c) 2011-2012, SpoutDev <http://www.spout.org/>
- * SpoutAPI is licensed under the SpoutDev License Version 1.
+ * Copyright (c) 2011-2012, VanillaDev <http://www.spout.org/>
+ * Vanilla is licensed under the SpoutDev License Version 1.
  *
- * SpoutAPI is free software: you can redistribute it and/or modify
+ * Vanilla is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -13,7 +13,7 @@
  * software, incorporating those changes, under the terms of the MIT license,
  * as described in the SpoutDev License Version 1.
  *
- * SpoutAPI is distributed in the hope that it will be useful,
+ * Vanilla is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.

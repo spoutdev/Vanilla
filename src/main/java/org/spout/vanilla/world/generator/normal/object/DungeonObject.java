4258df0655c61d48520eca636ac14eb05fc4eb54
narrowtux <narrow.m@gmail.com>
2012-07-19 16:09:03 +0200

    Forgot to increase chest count

--- src/main/java/org/spout/vanilla/world/generator/normal/object/DungeonObject.java
+++ src/main/java/org/spout/vanilla/world/generator/normal/object/DungeonObject.java
@@ -149,6 +149,7 @@ public class DungeonObject extends RandomObject {
 					continue;
 				}
 				chestObject.placeObject(w, xx, y, zz);
+				chestCount ++;
 				if (chestCount == maxNumberOfChests) {
 					break;
 				}

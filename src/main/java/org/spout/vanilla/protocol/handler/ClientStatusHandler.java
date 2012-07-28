f68c09c9aff1fb7c99cf3a321875878e7a52ab9e
Raphfrk <raphfrk@gmail.com>
2012-07-28 01:23:15 +0100

    Network sync should send the login packet

--- src/main/java/org/spout/vanilla/protocol/handler/ClientStatusHandler.java
+++ src/main/java/org/spout/vanilla/protocol/handler/ClientStatusHandler.java
@@ -29,25 +29,18 @@ package org.spout.vanilla.protocol.handler;
 import org.spout.api.Spout;
 import org.spout.api.event.Event;
 import org.spout.api.event.player.PlayerConnectEvent;
-import org.spout.api.geo.World;
 import org.spout.api.player.Player;
 import org.spout.api.protocol.MessageHandler;
 import org.spout.api.protocol.Session;
-import org.spout.vanilla.data.VanillaData;
 import org.spout.vanilla.protocol.VanillaProtocol;
 import org.spout.vanilla.protocol.msg.ClientStatusMessage;
-import org.spout.vanilla.protocol.msg.login.LoginRequestMessage;
 
 public class ClientStatusHandler extends MessageHandler<ClientStatusMessage> {
 	@Override
 	public void handleServer(Session session, Player player, ClientStatusMessage message) {
-		// TODO Do we handle anything? Should we send chunks when the client says "I am a'okay to log in?", what does "I am a'okay to login even mean?"
-		// For now lets print out when the client sends it
-		Spout.log("Client sent: " + message.toString());
-		World world = Spout.getEngine().getWorlds().iterator().next();
+		// TODO - need check if player has already connected
 		if (message.getStatus() == ClientStatusMessage.INITIAL_SPAWN) {
 			playerConnect(session, (String) session.getDataMap().get("username"));
-			session.send(false, true, new LoginRequestMessage(session.getPlayer().getEntity().getId(), world.getDataMap().get(VanillaData.WORLD_TYPE).getName(), world.getDataMap().get(VanillaData.GAMEMODE).getId(), (byte) world.getDataMap().get(VanillaData.DIMENSION).getId(), world.getDataMap().get(VanillaData.DIFFICULTY).getId(), (short) Spout.getEngine().getMaxPlayers()));
 		}
 
 	}

e2b321936de83d77584ddab32472588e838bd297
dredhorse <dredhorse@breiden.net>
2012-08-31 21:10:34 +0200

    added missing creatures
    Signed-off-by: dredhorse <dredhorse@breiden.net>

--- src/main/java/org/spout/vanilla/components/VanillaEntityTypes.java
+++ src/main/java/org/spout/vanilla/components/VanillaEntityTypes.java
@@ -26,10 +26,58 @@
  */
 package org.spout.vanilla.components;
 
+import org.spout.vanilla.components.types.BlazeType;
+import org.spout.vanilla.components.types.CaveSpiderType;
+import org.spout.vanilla.components.types.ChickenType;
+import org.spout.vanilla.components.types.CowType;
+import org.spout.vanilla.components.types.CreeperType;
 import org.spout.vanilla.components.types.EnderDragonType;
 import org.spout.vanilla.components.types.EndermanType;
+import org.spout.vanilla.components.types.GhastType;
+import org.spout.vanilla.components.types.GiantType;
+import org.spout.vanilla.components.types.IronGolemType;
+import org.spout.vanilla.components.types.MagmaCubeType;
+import org.spout.vanilla.components.types.MushroomCowType;
+import org.spout.vanilla.components.types.OcelotType;
+import org.spout.vanilla.components.types.PigType;
+import org.spout.vanilla.components.types.PigZombieType;
+import org.spout.vanilla.components.types.SheepType;
+import org.spout.vanilla.components.types.SilverfishType;
+import org.spout.vanilla.components.types.SkeletonType;
+import org.spout.vanilla.components.types.SlimeType;
+import org.spout.vanilla.components.types.SnowManType;
+import org.spout.vanilla.components.types.SpiderType;
+import org.spout.vanilla.components.types.SquidType;
+import org.spout.vanilla.components.types.VillagerType;
+import org.spout.vanilla.components.types.WolfType;
+import org.spout.vanilla.components.types.ZombieType;
 
 public class VanillaEntityTypes {
+		// creatures
+	public static final BlazeType TYPE_BLAZE = new BlazeType();
+	public static final CaveSpiderType TYPE_CAVESPIDER = new CaveSpiderType();
+	public static final ChickenType TYPE_CHICKEN = new ChickenType();
+	public static final CowType TYPE_COW = new CowType();
+	public static final CreeperType TYPE_CREEPER = new CreeperType();
 	public static final EnderDragonType TYPE_ENDERDRAGON = new EnderDragonType();
-	public static final EndermanType TYPE_ENDERMAN = new EndermanType();	
+	public static final EndermanType TYPE_ENDERMAN = new EndermanType();
+	public static final GhastType TYPE_GHAST = new GhastType();
+	public static final GiantType TYPE_GIANT = new GiantType();
+	public static final IronGolemType TYPE_IRONGOLEM = new IronGolemType();
+	public static final MagmaCubeType TYPE_MAGMACUBE = new MagmaCubeType();
+	public static final MushroomCowType TYPE_MUSHROOMCOW = new MushroomCowType();
+	public static final OcelotType TYPE_OCELOT = new OcelotType();
+	public static final PigType TYPE_PIG = new PigType();
+	public static final PigZombieType TYPE_PIGZOMBIE = new PigZombieType();
+	public static final SheepType TYPE_SHEEP = new SheepType();
+	public static final SilverfishType TYPE_SILVERFISH = new SilverfishType();
+	public static final SkeletonType TYPE_SKELETON = new SkeletonType();
+	public static final SlimeType TYPE_SLIME = new SlimeType();
+	public static final SnowManType TYPE_SNOWMAN = new SnowManType();
+	public static final SpiderType TYPE_SPIDER = new SpiderType();
+	public static final SquidType TYPE_SQUID = new SquidType();
+	public static final VillagerType TYPE_VILLAGER = new VillagerType();
+	public static final WolfType TYPE_WOLF = new WolfType();
+	public static final ZombieType TYPE_ZOMBIE = new ZombieType();
+
 }

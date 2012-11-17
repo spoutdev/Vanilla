6c3102a82368d34531ee1a17262802d5ca7eeee8
Afforess <afforess@gmail.com>
2012-11-17 14:42:30 -0500

    Right size

--- src/main/java/org/spout/vanilla/component/substance/Item.java
+++ src/main/java/org/spout/vanilla/component/substance/Item.java
@@ -45,7 +45,7 @@ public class Item extends EntityComponent {
 	@Override
 	public void onAttached() {
 		getOwner().getNetwork().setEntityProtocol(VanillaPlugin.VANILLA_PROTOCOL_ID, new ItemEntityProtocol());
-		getOwner().add(PhysicsComponent.class).setCollisionShape(new BoxShape(0.2F, 0.2F, 0.2F));
+		getOwner().add(PhysicsComponent.class).setCollisionShape(new BoxShape(0.25F, 0.25F, 0.25F));
 	}
 
 	@Override

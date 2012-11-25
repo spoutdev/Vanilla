478f316806b18efea2272411ee07cf9980f07b0f
Raphfrk <raphfrk@gmail.com>
2012-11-25 16:15:28 +0000

    Fixed settings for Snow block

--- src/main/java/org/spout/vanilla/material/block/misc/Snow.java
+++ src/main/java/org/spout/vanilla/material/block/misc/Snow.java
@@ -46,26 +46,30 @@ public class Snow extends GroundAttachable implements DynamicMaterial, Initializ
 	public static final Snow[] SNOW = new Snow[8];
 	
 	static {
-		SNOW[0] = new Snow("Snow", 78);
-		SNOW[1] = new Snow("Snow_1", 1, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_1.spm");
-		SNOW[2] = new Snow("Snow_2", 2, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_2.spm");
-		SNOW[3] = new Snow("Snow_3", 3, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_3.spm");
-		SNOW[4] = new Snow("Snow_4", 4, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_4.spm");
-		SNOW[5] = new Snow("Snow_5", 5, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_5.spm");
-		SNOW[6] = new Snow("Snow_6", 6, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_6.spm");
-		SNOW[7] = new Snow("Snow_7", 7, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_7.spm");
+		SNOW[0] = new Snow("Snow", 78, "model://Vanilla/resources/materials/block/nonsolid/snow/snow_1.spm");
+		SNOW[1] = new Snow("Snow_1", 1, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_2.spm");
+		SNOW[2] = new Snow("Snow_2", 2, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_3.spm");
+		SNOW[3] = new Snow("Snow_3", 3, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_4.spm");
+		SNOW[4] = new Snow("Snow_4", 4, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_5.spm");
+		SNOW[5] = new Snow("Snow_5", 5, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_6.spm");
+		SNOW[6] = new Snow("Snow_6", 6, SNOW[0], "model://Vanilla/resources/materials/block/nonsolid/snow/snow_7.spm");
+		SNOW[7] = new Snow("Snow_7", 7, SNOW[0], "model://Vanilla/resources/materials/block/solid/snowblock/snowblock.spm");
 	}
 
-	public Snow(String name, int id) {
-		super((short) 0x07, name, id, "model://Vanilla/resources/materials/block/solid/snowblock/snowblock.spm");
-		this.setLiquidObstacle(false).setStepSound(SoundEffects.STEP_CLOTH).setHardness(0.1F).setResistance(0.2F).setOpaque();
+	public Snow(String name, int id, String model) {
+		super((short) 0x07, name, id, model);
+		this.setLiquidObstacle(false).setStepSound(SoundEffects.STEP_CLOTH).setHardness(0.1F).setResistance(0.2F).setTransparent();
+		this.setOcclusion((short) 0, BlockFace.BOTTOM);
 		this.addMiningType(ToolType.SPADE).setMiningLevel(ToolLevel.WOOD);
 	}
 	
 	private Snow(String name, int data, Snow parent, String model) {
 		super(name, SNOW[0].getId(), data, parent, model);
-		this.setLiquidObstacle(false).setStepSound(SoundEffects.STEP_CLOTH).setHardness(0.1F).setResistance(0.2F).setOpaque();
+		this.setLiquidObstacle(false).setStepSound(SoundEffects.STEP_CLOTH).setHardness(0.1F).setResistance(0.2F).setTransparent();
 		this.setOcclusion((short) 0, BlockFace.BOTTOM);
+		if (data == 7) {
+			this.setOpaque();
+		}
 		this.addMiningType(ToolType.SPADE).setMiningLevel(ToolLevel.WOOD);
 	}
 

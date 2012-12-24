0d73eac41ea26be82e3b3c8995f5d1b4def8da9e
Afforess <afforess@gmail.com>
2012-12-23 21:52:22 -0500

    Fix byte count on EntityObjectMessage

--- src/main/java/org/spout/vanilla/protocol/msg/entity/spawn/EntityObjectMessage.java
+++ src/main/java/org/spout/vanilla/protocol/msg/entity/spawn/EntityObjectMessage.java
@@ -30,23 +30,27 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import org.spout.api.entity.Entity;
 import org.spout.api.geo.discrete.Point;
+import org.spout.api.geo.discrete.Transform;
 import org.spout.api.math.Vector3;
 import org.spout.api.protocol.reposition.RepositionManager;
 import org.spout.api.util.SpoutToStringStyle;
 
 import org.spout.vanilla.component.substance.object.ObjectEntity;
+import org.spout.vanilla.protocol.ChannelBufferUtils;
 import org.spout.vanilla.protocol.msg.entity.EntityMessage;
 
 public final class EntityObjectMessage extends EntityMessage {
 	private final byte type;
 	private final int x, y, z, throwerId;
 	private final short speedX, speedY, speedZ;
+	private final byte pitch, yaw;
 
 	public EntityObjectMessage(Entity entity, byte type, int throwerId, RepositionManager rm) {
 		super(entity);
 		this.type = type;
 		this.throwerId = throwerId;
-		Point pos = entity.getTransform().getPosition();
+		Transform transform = entity.getTransform().getTransform();
+		Point pos = transform.getPosition();
 
 		double p = 32d;
 		x = rm.convertX((int) Math.floor(pos.getX() * p));
@@ -62,13 +66,16 @@ public final class EntityObjectMessage extends EntityMessage {
 		speedX = (short) (velocity.getX() * s);
 		speedY = (short) (velocity.getY() * s);
 		speedZ = (short) (velocity.getZ() * s);
+		
+		pitch = (byte) ChannelBufferUtils.protocolifyRotation(transform.getRotation().getPitch());
+		yaw = (byte) ChannelBufferUtils.protocolifyRotation(transform.getRotation().getYaw());
 	}
 
 	public EntityObjectMessage(Entity entity, byte type, RepositionManager rm) {
 		this(entity, type, 0, rm);
 	}
 
-	public EntityObjectMessage(int entityId, byte type, int x, int y, int z, int throwerId, short speedX, short speedY, short speedZ, RepositionManager rm) {
+	public EntityObjectMessage(int entityId, byte type, int x, int y, int z, int throwerId, short speedX, short speedY, short speedZ, byte yaw, byte pitch, RepositionManager rm) {
 		super(entityId);
 		this.type = type;
 		this.x = rm.convertX(x);
@@ -78,10 +85,12 @@ public final class EntityObjectMessage extends EntityMessage {
 		this.speedX = speedX;
 		this.speedY = speedY;
 		this.speedZ = speedZ;
+		this.pitch = pitch;
+		this.yaw = yaw;
 	}
 
-	public EntityObjectMessage(int entityId, byte type, int x, int y, int z, int throwerId, RepositionManager rm) {
-		this(entityId, type, x, y, z, throwerId, (short) 0, (short) 0, (short) 0, rm);
+	public EntityObjectMessage(int entityId, byte type, int x, int y, int z, int throwerId, byte yaw, byte pitch, RepositionManager rm) {
+		this(entityId, type, x, y, z, throwerId, (short) 0, (short) 0, (short) 0, pitch, yaw, rm);
 	}
 
 	public byte getType() {
@@ -116,6 +125,14 @@ public final class EntityObjectMessage extends EntityMessage {
 		return speedZ;
 	}
 
+	public byte getPitch() {
+		return pitch;
+	}
+
+	public byte getYaw() {
+		return yaw;
+	}
+
 	@Override
 	public String toString() {
 		return new ToStringBuilder(this, SpoutToStringStyle.INSTANCE)
@@ -127,6 +144,8 @@ public final class EntityObjectMessage extends EntityMessage {
 				.append("speedX", speedX)
 				.append("speedY", speedY)
 				.append("speedZ", speedZ)
+				.append("pitch", pitch)
+				.append("yaw", yaw)
 				.toString();
 	}
 
@@ -147,7 +166,9 @@ public final class EntityObjectMessage extends EntityMessage {
 				.append(this.z, other.z)
 				.append(this.speedX, other.speedX)
 				.append(this.speedY, other.speedY)
-				.append(this.speedZ, speedZ)
+				.append(this.speedZ, other.speedZ)
+				.append(this.yaw, other.yaw)
+				.append(this.pitch, other.pitch)
 				.isEquals();
 	}
 }

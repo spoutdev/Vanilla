13d71a8ce360ea1652173408a3afafec3116551c
Afforess <afforess@gmail.com>
2013-01-05 23:15:06 -0500

    Fix grass spreading

--- src/main/java/org/spout/vanilla/material/block/solid/Grass.java
+++ src/main/java/org/spout/vanilla/material/block/solid/Grass.java
@@ -73,6 +73,6 @@ public class Grass extends SpreadingSolid implements DynamicMaterial, Initializa
 
 	@Override
 	public long getSpreadingTime(Block b) {
-		return 120000L + MathHelper.getRandom().nextInt(60000) * 5;
+		return 60000L + MathHelper.getRandom().nextInt(60000) * 3;
 	}
 }

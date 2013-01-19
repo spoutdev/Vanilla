38f80af148f5c814944f1d2d82b557c181a75bf9
Aleksi Sapon <alek42008@yahoo.com>
2013-01-19 16:30:57 -0500

    Improve the Whittaker layer
    Signed-off-by: Aleksi Sapon <QcTechs@gmail.com>

--- src/main/java/org/spout/vanilla/plugin/world/generator/normal/biome/selector/WhittakerLayer.java
+++ src/main/java/org/spout/vanilla/plugin/world/generator/normal/biome/selector/WhittakerLayer.java
@@ -33,48 +33,74 @@ import java.util.List;
 
 import net.royawesome.jlibnoise.NoiseQuality;
 import net.royawesome.jlibnoise.module.modifier.Clamp;
+import net.royawesome.jlibnoise.module.modifier.ScaleBias;
 import net.royawesome.jlibnoise.module.source.Perlin;
 
 import org.spout.api.generator.biome.selector.BiomeSelectorLayer;
 import org.spout.api.generator.biome.selector.LayeredBiomeSelectorElement;
+import org.spout.api.math.Vector2;
 
 public class WhittakerLayer implements BiomeSelectorLayer {
-	private final Perlin humidityPerlin = new Perlin();
-	private final Clamp humidity = new Clamp();
 	private final Perlin temperaturePerlin = new Perlin();
-	private final Clamp temperature = new Clamp();
+	private final Clamp temperatureClamp = new Clamp();
+	private final ScaleBias temperature = new ScaleBias();
+	private final Perlin humidityPerlin = new Perlin();
+	private final Clamp humidityClamp = new Clamp();
+	private final ScaleBias humidity = new ScaleBias();
+	private double minTemperature = -20;
+	private double maxTemperature = 30;
+	private double minHumidity = 0;
+	private double maxHumidity = 400;
 	private final int uniquenessValue;
 	private final List<WhittakerElement> elements = new ArrayList<WhittakerElement>();
 
 	public WhittakerLayer(int uniquenessValue) {
 		this.uniquenessValue = uniquenessValue;
-		humidity.SetSourceModule(0, humidityPerlin);
-		humidity.setLowerBound(-1);
-		humidity.setUpperBound(1);
-		temperature.SetSourceModule(0, temperaturePerlin);
-		temperature.setLowerBound(-1);
-		temperature.setUpperBound(1);
+		temperatureClamp.SetSourceModule(0, temperaturePerlin);
+		temperatureClamp.setLowerBound(-1);
+		temperatureClamp.setUpperBound(1);
+		temperature.SetSourceModule(0, temperatureClamp);
+		temperature.setScale(0.5);
+		temperature.setBias(0.5);
+		humidityClamp.SetSourceModule(0, humidityPerlin);
+		humidityClamp.setLowerBound(-1);
+		humidityClamp.setUpperBound(1);
+		humidity.SetSourceModule(0, humidityClamp);
+		humidity.setScale(0.5);
+		humidity.setBias(0.5);
 	}
 
 	@Override
 	public LayeredBiomeSelectorElement pick(int x, int y, int z, long seed) {
-		humidityPerlin.setSeed((int) seed * uniquenessValue);
 		temperaturePerlin.setSeed((int) seed * uniquenessValue * uniquenessValue);
-		final double humidityValue = humidity.GetValue(x, y, z);
+		humidityPerlin.setSeed((int) seed * uniquenessValue);
 		final double temperatureValue = temperature.GetValue(x, y, z);
-		for (WhittakerElement element : elements) {
-			if (humidityValue >= element.getMinHumidity() && humidityValue <= element.getMaxHumidity()
-					&& temperatureValue >= element.getMinTemperature() && temperatureValue <= element.getMaxTemperature()) {
-				return element.getElement();
-			}
-		}
-		return null;
+		final double humidityValue = humidity.GetValue(x, y, z);
+		return findClosest(humidityValue * temperatureValue * (minTemperature - maxTemperature) + maxTemperature,
+				(1 - humidityValue) * (maxHumidity - minHumidity) + minHumidity);
 	}
 
-	public WhittakerLayer addElement(LayeredBiomeSelectorElement element,
-									 float minHumidity, float maxHumidity, float minTemperature, float maxTemperature) {
-		return addElement(new WhittakerElement(element, minHumidity, maxHumidity,
-				minTemperature, maxTemperature));
+	private LayeredBiomeSelectorElement findClosest(double temperature, double humidity) {
+		final Vector2 position = new Vector2(temperature, humidity);
+		WhittakerElement closestElement = null;
+		float smallestDistance = 0;
+		for (WhittakerElement element : elements) {
+			final float distance = element.getPosition().subtract(position).lengthSquared();
+			if (closestElement == null) {
+				closestElement = element;
+				smallestDistance = distance;
+			} else {
+				if (distance <= smallestDistance) {
+					closestElement = element;
+					smallestDistance = distance;
+				}
+			}
+		}
+		return closestElement == null ? null : closestElement.getElement();
+	}
+
+	public WhittakerLayer addElement(LayeredBiomeSelectorElement element, float temperature, float humidity) {
+		return addElement(new WhittakerElement(element, temperature, humidity));
 	}
 
 	public WhittakerLayer addElement(WhittakerElement element) {
@@ -88,31 +114,6 @@ public class WhittakerLayer implements BiomeSelectorLayer {
 
 	public WhittakerLayer addElements(Collection<WhittakerElement> elements) {
 		this.elements.addAll(elements);
-		return this;
-	}
-
-	public WhittakerLayer setHumidityFrequency(double frequency) {
-		humidityPerlin.setFrequency(frequency);
-		return this;
-	}
-
-	public WhittakerLayer setHumidityLacunarity(double lacunarity) {
-		humidityPerlin.setLacunarity(lacunarity);
-		return this;
-	}
-
-	public WhittakerLayer setHumidityNoiseQuality(NoiseQuality quality) {
-		humidityPerlin.setNoiseQuality(quality);
-		return this;
-	}
-
-	public WhittakerLayer setHumidityOctaveCount(int octaveCount) {
-		humidityPerlin.setOctaveCount(octaveCount);
-		return this;
-	}
-
-	public WhittakerLayer setHumidityPersistence(double persistence) {
-		humidityPerlin.setPersistence(persistence);
 		return this;
 	}
 
@@ -141,57 +142,80 @@ public class WhittakerLayer implements BiomeSelectorLayer {
 		return this;
 	}
 
+	public WhittakerLayer setHumidityFrequency(double frequency) {
+		humidityPerlin.setFrequency(frequency);
+		return this;
+	}
+
+	public WhittakerLayer setHumidityLacunarity(double lacunarity) {
+		humidityPerlin.setLacunarity(lacunarity);
+		return this;
+	}
+
+	public WhittakerLayer setHumidityNoiseQuality(NoiseQuality quality) {
+		humidityPerlin.setNoiseQuality(quality);
+		return this;
+	}
+
+	public WhittakerLayer setHumidityOctaveCount(int octaveCount) {
+		humidityPerlin.setOctaveCount(octaveCount);
+		return this;
+	}
+
+	public WhittakerLayer setHumidityPersistence(double persistence) {
+		humidityPerlin.setPersistence(persistence);
+		return this;
+	}
+
+	public WhittakerLayer setMinTemperature(double minTemperature) {
+		this.minTemperature = minTemperature;
+		return this;
+	}
+
+	public WhittakerLayer setMaxTemperature(double maxTemperature) {
+		this.maxTemperature = maxTemperature;
+		return this;
+	}
+
+	public WhittakerLayer setMinHumidity(double minHumidity) {
+		this.minHumidity = minHumidity;
+		return this;
+	}
+
+	public WhittakerLayer setMaxHumidity(double maxHumidity) {
+		this.maxHumidity = maxHumidity;
+		return this;
+	}
+
 	public static class WhittakerElement {
 		private final LayeredBiomeSelectorElement element;
-		private float minHumidity;
-		private float maxHumidity;
-		private float minTemperature;
-		private float maxTemperature;
+		private float temperature;
+		private float humidity;
 
 		public WhittakerElement(LayeredBiomeSelectorElement element) {
 			this.element = element;
 		}
 
-		public WhittakerElement(LayeredBiomeSelectorElement element,
-								float minHumidity, float maxHumidity,
-								float minTemperature, float maxTemperature) {
+		public WhittakerElement(LayeredBiomeSelectorElement element, float temperature, float humidity) {
 			this(element);
-			this.minHumidity = minHumidity;
-			this.maxHumidity = maxHumidity;
-			this.minTemperature = minTemperature;
-			this.maxTemperature = maxTemperature;
-		}
-
-		public WhittakerElement setHumidity(float min, float max) {
-			this.minHumidity = min;
-			this.maxHumidity = max;
-			return this;
-		}
-
-		public WhittakerElement setTemperature(float min, float max) {
-			this.minTemperature = min;
-			this.maxTemperature = max;
-			return this;
+			this.temperature = temperature;
+			this.humidity = humidity;
 		}
 
 		public LayeredBiomeSelectorElement getElement() {
 			return element;
 		}
 
-		public float getMinHumidity() {
-			return minHumidity;
+		public float getTemperature() {
+			return temperature;
 		}
 
-		public float getMaxHumidity() {
-			return maxHumidity;
+		public float getHumidity() {
+			return humidity;
 		}
 
-		public float getMinTemperature() {
-			return minTemperature;
-		}
-
-		public float getMaxTemperature() {
-			return maxTemperature;
+		public Vector2 getPosition() {
+			return new Vector2(temperature, humidity);
 		}
 	}
 }

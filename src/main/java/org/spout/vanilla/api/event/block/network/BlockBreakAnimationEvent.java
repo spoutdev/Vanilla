273997d8b2588183929d59bb02e8b4941b102d06
Sleaker <sleaker@gmail.com>
2013-01-21 19:30:18 -0800

    not sure how that happened
    Signed-off-by: Sleaker <sleaker@gmail.com>

--- src/main/java/org/spout/vanilla/api/event/block/network/BlockBreakAnimationEvent.java
+++ src/main/java/org/spout/vanilla/api/event/block/network/BlockBreakAnimationEvent.java
@@ -24,7 +24,7 @@
  * License and see <http://spout.in/licensev1> for the full license, including
  * the MIT license.
  */
-package org.spout.vanilla.plugin.event.block.network;
+package org.spout.vanilla.api.event.block.network;
 
 import org.spout.api.entity.Entity;
 import org.spout.api.geo.discrete.Point;

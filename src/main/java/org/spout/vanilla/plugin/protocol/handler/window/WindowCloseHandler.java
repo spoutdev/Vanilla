77741173620db50cc79c3e5c3e332d5b43278c1f
Afforess <afforess@gmail.com>
2013-02-02 22:54:18 -0500

    NPE fix when closing player inventory without touching any items

--- src/main/java/org/spout/vanilla/plugin/protocol/handler/window/WindowCloseHandler.java
+++ src/main/java/org/spout/vanilla/plugin/protocol/handler/window/WindowCloseHandler.java
@@ -42,10 +42,13 @@ public final class WindowCloseHandler extends MessageHandler<WindowCloseMessage>
 		if (!session.hasPlayer()) {
 			return;
 		}
-		if (session.getPlayer().get(WindowHolder.class).getActiveWindow() instanceof ChestWindow) {
-			SoundEffects.RANDOM_CHESTCLOSED.playGlobal(session.getPlayer().getScene().getPosition());
+		WindowHolder window = session.getPlayer().get(WindowHolder.class);
+		if (window != null) {
+			if (window.getActiveWindow() instanceof ChestWindow) {
+				SoundEffects.RANDOM_CHESTCLOSED.playGlobal(session.getPlayer().getScene().getPosition());
+			}
+			window.closeWindow();
 		}
-		session.getPlayer().get(WindowHolder.class).closeWindow();
 	}
 
 	@Override

a8a7d08abce6172ca0054450f0d45a4beee74fc6
Karang <arthur.hennequin@free.fr>
2013-02-13 21:25:24 +0100

    Continuation of client connection to server

--- src/main/java/org/spout/vanilla/plugin/protocol/ClientLoginAuth.java
+++ src/main/java/org/spout/vanilla/plugin/protocol/ClientLoginAuth.java
@@ -1,20 +1,50 @@
+/*
+ * This file is part of Vanilla.
+ *
+ * Copyright (c) 2011-2012, Spout LLC <http://www.spout.org/>
+ * Vanilla is licensed under the Spout License Version 1.
+ *
+ * Vanilla is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * In addition, 180 days after any changes are published, you can use the
+ * software, incorporating those changes, under the terms of the MIT license,
+ * as described in the Spout License Version 1.
+ *
+ * Vanilla is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the MIT license and the Spout License Version 1 along with this program.
+ * If not, see <http://www.gnu.org/licenses/> for the GNU Lesser General Public
+ * License and see <http://spout.in/licensev1> for the full license, including
+ * the MIT license.
+ */
 package org.spout.vanilla.plugin.protocol;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.spout.api.protocol.Session;
+import org.spout.api.Spout;
+import org.spout.api.scheduler.TaskPriority;
 import org.spout.vanilla.plugin.VanillaPlugin;
 
 public class ClientLoginAuth implements Runnable {
 	private static final String baseURL = "http://session.minecraft.net/game/joinserver.jsp?";
-	private final Session session;
+	private static final String authString = "OK";
 	private final String params;
+	private final Runnable runnable;
 	
-	public ClientLoginAuth(Session session, String hash) {
-		this.session = session;
+	public ClientLoginAuth(String hash, Runnable runnable) {
 		VanillaPlugin p = VanillaPlugin.getInstance();
 		this.params = "user="+p.getUsername()+"&sessionId="+p.getSessionId()+"&serverId="+hash;
+		this.runnable = runnable;
 	}
 	
 	@Override
@@ -25,8 +55,20 @@ public class ClientLoginAuth implements Runnable {
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setConnectTimeout(30000);
 			connection.setReadTimeout(30000);
-			//TODO: falling asleep, i'll continue these later if someone haven't done it
+			connection.connect();
 			
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String reply = in.readLine();
+			
+			if (Spout.getEngine().debugMode()) {
+				Spout.getLogger().info("Logging in "+reply);
+			}
+			
+			if (reply.equals(authString)) {
+				Spout.getEngine().getScheduler().scheduleSyncDelayedTask(VanillaPlugin.getInstance(), runnable, TaskPriority.CRITICAL);
+			} else {
+				Spout.getLogger().info("Error while logging in "+reply);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
--- src/main/java/org/spout/vanilla/plugin/protocol/handler/auth/EncryptionKeyRequestHandler.java
+++ src/main/java/org/spout/vanilla/plugin/protocol/handler/auth/EncryptionKeyRequestHandler.java
@@ -26,20 +26,84 @@
  */
 package org.spout.vanilla.plugin.protocol.handler.auth;
 
+import java.security.MessageDigest;
+import java.util.Random;
+
+import org.bouncycastle.crypto.AsymmetricBlockCipher;
+import org.bouncycastle.crypto.InvalidCipherTextException;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.params.KeyParameter;
 import org.spout.api.protocol.MessageHandler;
 import org.spout.api.protocol.Session;
+import org.spout.api.security.SecurityHandler;
+import org.spout.vanilla.plugin.configuration.VanillaConfiguration;
 import org.spout.vanilla.plugin.protocol.ClientLoginAuth;
 import org.spout.vanilla.plugin.protocol.msg.auth.EncryptionKeyRequestMessage;
+import org.spout.vanilla.plugin.protocol.msg.auth.EncryptionKeyResponseMessage;
 
 public class EncryptionKeyRequestHandler extends MessageHandler<EncryptionKeyRequestMessage> {
 	@Override
 	public void handleClient(final Session session, final EncryptionKeyRequestMessage message) {
 		System.out.println(message.toString());
 		
-		//TODO: Sha1 hash
-		String hash = "";
+		final byte[] sharedSecret = generateSymetricKey();
 		
-		Thread loginAuth = new Thread(new ClientLoginAuth(session, hash));
-		loginAuth.start();
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				String keyAlgorithm = VanillaConfiguration.ENCRYPT_KEY_ALGORITHM.getString();
+				String keyPadding = VanillaConfiguration.ENCRYPT_KEY_PADDING.getString();
+				AsymmetricBlockCipher cipher = SecurityHandler.getInstance().getAsymmetricCipher(keyAlgorithm, keyPadding);
+				//TODO: need to be changed
+				cipher.init(SecurityHandler.ENCRYPT_MODE, new KeyParameter(message.getSecretArray()));
+				
+				try {
+					byte[] encodedSecret = cipher.processBlock(sharedSecret, 0, 16);
+					byte[] encodedToken = cipher.processBlock(message.getVerifyTokenArray(), 0, 4);
+					
+					session.send(true, true, new EncryptionKeyResponseMessage(false, encodedSecret, encodedToken));
+				} catch (InvalidCipherTextException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.reset();
+			md.update(message.getSessionId().getBytes("ISO_8859_1"));
+			md.update(sharedSecret);
+			md.update(message.getVerifyTokenArray());
+			
+			String hash = convertToHex(md.digest());
+			
+			Thread loginAuth = new Thread(new ClientLoginAuth(hash, runnable));
+			loginAuth.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String convertToHex(byte[] data) {
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < data.length; i++) {
+			int halfbyte = (data[i] >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do {
+				if ((0 <= halfbyte) && (halfbyte <= 9))
+					buf.append((char) ('0' + halfbyte));
+				else
+					buf.append((char) ('a' + (halfbyte - 10)));
+				halfbyte = data[i] & 0x0F;
+			} while (two_halfs++ < 1);
+		}
+		return buf.toString();
+	}
+	
+	private byte[] generateSymetricKey() {
+		final byte[] key = new byte[16];
+		final Random rand = new Random();
+		rand.nextBytes(key);
+		return key;
 	}
 }

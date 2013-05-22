8595a690f553422f6caef08335546a8c12e83d2f
Waterpicker <Waterpicker@rocketmail.com>
2013-05-21 23:54:52 -0500

    Fixed Input

--- src/main/java/org/spout/vanilla/input/VanillaInputExecutor.java
+++ src/main/java/org/spout/vanilla/input/VanillaInputExecutor.java
@@ -33,6 +33,7 @@ import org.spout.api.geo.discrete.Transform;
 import org.spout.api.input.InputExecutor;
 import org.spout.api.math.QuaternionMath;
 import org.spout.api.math.Vector3;
+import org.spout.vanilla.component.entity.misc.Head;
 
 public class VanillaInputExecutor implements InputExecutor {
 	private final Player player;
@@ -68,7 +69,8 @@ public class VanillaInputExecutor implements InputExecutor {
 			offset = offset.subtract(ts.upVector().multiply(speed).multiply(dt));
 		}
 
-		ts.translateAndSetRotation(offset, QuaternionMath.rotation(inputState.pitch(), inputState.yaw(), ts.getRotation().getRoll()));
+		ts.translateAndSetRotation(offset, QuaternionMath.rotation(ts.getRotation().getPitch(), inputState.yaw(), ts.getRotation().getRoll()));
+		player.get(Head.class).setRotation(QuaternionMath.rotation(inputState.pitch(), inputState.yaw(), ts.getRotation().getRoll()));
 		sc.setTransform(ts);
 	}
 }

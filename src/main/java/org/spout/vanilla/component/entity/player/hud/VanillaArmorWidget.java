2ca7cb91779201adf152b138d4f7efa9e992f551
Steven Downer <grinch@outlook.com>
2013-06-02 00:02:24 -0500

    Adjustments to Vanilla Armor GUI - Default armor value now returns 0 instead of 15 - Adjusted VanillaArmorWidget a bit to be easier to work with - Adjusted Height and Width values for armor source - Removed old code from Drowning.java Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/org/spout/vanilla/component/entity/player/hud/VanillaArmorWidget.java
+++ src/main/java/org/spout/vanilla/component/entity/player/hud/VanillaArmorWidget.java
@@ -39,6 +39,10 @@ import org.spout.vanilla.data.VanillaRenderMaterials;
 
 public class VanillaArmorWidget extends ArmorWidget {
 	private final RenderPartPack armorPack = new RenderPartPack(VanillaRenderMaterials.ICONS_MATERIAL);
+	private final Rectangle armorFull = new Rectangle(43f / 256f, 9f / 256f, 9f / 256f, 10f / 256f); // Full icon
+	private final Rectangle armorPart = new Rectangle(25f / 256f, 9f / 256f, 9f / 256f, 10f / 256f); // Part icon
+	private final Rectangle armorSlot = new Rectangle(16f / 256f, 9f / 256f, 9f / 256f, 10f / 256f); // Slot icon
+	private final Rectangle armorEmpty = new Rectangle(52f / 256f, 9f / 256f, 9f / 256f, 10f / 256f); // No icon
 
 	@Override
 	public void init(Widget armor, HUD hud) {
@@ -51,7 +55,7 @@ public class VanillaArmorWidget extends ArmorWidget {
 			final RenderPart armorPart = new RenderPart();
 			armorPart.setColor(Color.WHITE);
 			armorPart.setSprite(new Rectangle(x, -0.7f, 0.06f * SCALE, 0.06f));
-			armorPart.setSource(new Rectangle(52f / 256f, 9f / 256f, 12f / 256f, 12f / 256f));
+			armorPart.setSource(armorEmpty);
 			armorPack.add(armorPart);
 			x += dx;
 		}
@@ -66,21 +70,20 @@ public class VanillaArmorWidget extends ArmorWidget {
 
 		if (amount == 0) {
 			for (RenderPart armorPart : armorPack.getRenderParts()) {
-				armorPart.setSource(new Rectangle(52f / 256f, 9f / 256f, 12f / 256f, 12f / 256f)); // No icon
+				armorPart.setSource(armorEmpty);
 			}
 		} else {
 			for (int i = 0; i < 10; i++) {
 				float x = 0;
 				if (amount >= 2) {
-					x = 43f; // Full
+					armorPack.get(i).setSource(armorFull);
 					amount -= 2;
 				} else if (amount == 1) {
-					x = 25f; // Half
+					armorPack.get(i).setSource(armorPart);
 					amount = 0;
 				} else if (amount == 0) {
-					x = 16f; // Empty
+					armorPack.get(i).setSource(armorSlot);
 				}
-				armorPack.get(i).setSource(new Rectangle(x / 256f, 9f / 256f, 9f / 256f, 9f / 256f));
 			}
 		}
 		widget.update();
